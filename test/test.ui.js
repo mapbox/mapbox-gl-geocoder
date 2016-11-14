74c0737fa81fb684ec897fc9fd8712f675c46029
Tom MacWright <tom@macwright.org>
2016-11-14 17:37:33 -0500

    Fixes #83. Fixes remaining tests

--- test/test.ui.js
+++ test/test.ui.js
@@ -27,41 +27,31 @@ test('Geocoder#inputControl', function(tt) {
 
   tt.test('input', function(t) {
     setup();
-    t.plan(10);
     var inputEl = container.querySelector('.mapboxgl-ctrl-geocoder input');
     var clearEl = container.querySelector('.mapboxgl-ctrl-geocoder button');
-    inputEl.addEventListener('change', once(function() {
+
+    geocoder.query('-79,43');
+
+    geocoder.on('loading', once(function() {
+      t.pass('load event was emitted');
+    }));
+
+    geocoder.on('result', once(function() {
       t.ok(inputEl.value, 'value populates in input');
-      t.ok(clearEl.classList.contains('active'), 'clear link is active');
       clearEl.dispatchEvent(clickEvent);
     }));
 
-    geocoder.on('loading', once(function() {
-      t.ok('load event was emitted');
-    }));
-
     geocoder.on('clear', once(function() {
-      t.ok('input was cleared');
-      t.notOk(clearEl.classList.contains('active'), 'clear link removed');
+      t.pass('input was cleared');
 
       geocoder.setInput('Paris');
       t.equals(inputEl.value, 'Paris', 'value populates in input');
-      t.ok(clearEl.classList.contains('active'), 'clear link is active');
 
-      geocoder.setInput([90, 45]);
+      geocoder.setInput('90,45');
       t.equals(inputEl.value, '90,45', 'valid LngLat value populates in input');
-
-      geocoder.setInput([270, 45]);
-      t.equals(inputEl.value, '-90,45', 'LngLat wraps if need be');
+      t.end();
     }));
 
-    geocoder.query([-79, 43]);
-  });
-
-  tt.test('position', function(t) {
-    setup({ position: 'bottom-left' });
-    t.ok(map.getContainer().querySelector('.mapboxgl-ctrl-bottom-left .mapboxgl-ctrl-geocoder'));
-    t.end();
   });
 
   tt.test('placeholder', function(t) {
